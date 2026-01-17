185e283f9c153eafde15772605ae348ba4dc97ee
Abhideep Gupta <abhideep.gupta.g@gmail.com>
2026-01-17 17:59:43 +0530

    Add React frontend and integrate with Spring Boot

--- src/main/java/com/example/student/controller/StudentController.java
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/students")
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
     private final StudentService studentService;
